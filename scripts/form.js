3d47629a28e9850916ed92f244dec85f62632285
Felix Ng <felixng@Felixs-MacBook-Pro.local>
2021-11-08 15:03:14 -0800

    some more form.js work

--- scripts/form.js
+++ scripts/form.js
@@ -4,8 +4,6 @@
 const app = firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 
-// Reference message collection
-var activity.
 
 // Listen for form submission
 document.getElementById('').addEventListener('', submitForm);
@@ -20,9 +18,23 @@ function submitForm(e){
     var time = getInputVal('');
     var location = getInputVal('');
 
+    // Save activity
+    saveActivity(name, description, time, location);
+
+
 }
 
 // Function to retrieve the form's values
 function getInputVal(id){
     return document.getElementById(id).value; 
+}
+
+// Save activity to firebase
+function saveActivity(name, description, time, location){
+    var newActivityRed = db.collection("Hobbies").doc(category).set({
+        name: name,
+        description: description,
+        time:  time,
+        location: location
+    })
 }
