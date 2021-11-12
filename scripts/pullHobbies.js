32649b8f6ec767b603c234e8dd8e884c5b82c647
Gary Au <tcgaryau@gmail.com>
2021-11-11 18:06:16 -0800

    Added Browse

--- scripts/pullHobbies.js
+++ scripts/pullHobbies.js
@@ -1,25 +1,59 @@
-/*
-function displayHikes() {
-  db.collection("Hikes")
-    .get()
-    .then((allHikes) => {
-      allHikes.forEach((doc) => {
-        var hikeName = doc.data().name; //gets the name field
-        var hikeID = doc.data().code; //gets the unique ID field
-        document.getElementById(hikeID).innerText = hikeName;
-      });
-    });
-}
-*/
+const container = document.querySelector(".container");
 
-var Count = 0;
+// Store last document
+let lastDoc = null;
 
-function getCollectionSize() {
-  db.collection("Hobbies")
-    .get()
-    .then((snap) => {
-      size = snap.size; // will return the collection size
-      console.log(size);
-    });
-}
+const getNextHobbies = async () => {
+  ref = db
+    .collection("TestHobbies")
+    .orderBy("name")
+    .startAfter(lastDoc || 0)
+    .limit(3);
 
+  const data = await ref.get();
+
+  // output
+  let template = "";
+  data.docs.forEach((doc) => {
+    const hobbies = doc.data();
+    template += `
+      <div class="card mb-3">
+        <div class="row g-0 pt-5">
+          <div class="col-md-4">
+            <img
+              src="./images/cartoongirlrope.jpg"
+              class="img-fluid rounded-start"
+              alt="rope"
+            />
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">${hobbies.name}</h5>
+              <p class="card-text">Description:<br>${hobbies.description}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+  });
+
+  container.innerHTML += template;
+
+  lastDoc = data.docs[data.docs.length - 1];
+
+  if (data.empty) {
+    loadMore.removeEventListener("click", handleClick);
+    document.getElementById("buttonContainer").innerHTML = "";
+  }
+};
+
+// Wait for DOM to load
+window.addEventListener("DOMContentLoaded", () => getNextHobbies());
+
+const loadMore = document.getElementById("loadMore");
+
+const handleClick = () => {
+  getNextHobbies();
+};
+
+loadMore.addEventListener("click", handleClick);
