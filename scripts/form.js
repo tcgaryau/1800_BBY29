85526a847ce73715537d089e7472ec2fc0255b40
Felix Ng <felixng@Felixs-MacBook-Pro.local>
2021-11-11 17:35:58 -0800

    added functionality for creating events

--- scripts/form.js
+++ scripts/form.js
@@ -4,30 +4,47 @@
 //const app = firebase.initializeApp(firebaseConfig);
 //const db = firebase.firestore();
 
+var currentUser;
+function submitForm() {
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      //if user is logged in
+      currentUser = db.collection("users").doc(user.uid);
+      var userID = user.uid;
+      console.log(currentUser);
+      console.log("user pressed submit");
 
-function submitForm(){
-    console.log("user pressed submit")
+      let newActivity = db.collection("Hobbies");
 
-    let newActivity = db.collection("Hobbies");
+      // Retrieve the values
 
-    // Retrieve the values
-    
-    let aName = document.getElementById('activityName').value;
-    //let category = document.getElementById('activityCategory');
-    let description = document.getElementById('descriptionText').value;
-    let time = document.getElementById('datetimepickerExample').value;
-    let location = document.getElementById('address').value;
-    let ZIP = document.getElementById('zip').value;
+      let x = document.getElementById("activityCategory");
+      let y = document.getElementById("city");
 
-    console.log(aName);
+      let aName = document.getElementById("activityName").value;
+      var category = x.options[x.selectedIndex].text;
+      let description = document.getElementById("descriptionText").value;
+      let time = document.getElementById("datetimepickerExample").value;
+      let location = document.getElementById("address").value;
+      var city = y.options[y.selectedIndex].value;
+      let ZIP = document.getElementById("zip").value;
 
-    newActivity.add({
+      console.log(aName);
+      console.log(city);
+
+      newActivity.add({
         name: aName,
-        //category: category,
+        category: category,
         description: description,
-        time:  time,
+        time: time,
         location: location,
-        ZIP: ZIP
-    })
+        province: city,
+        postalCode: ZIP,
+        host: userID,
+        
+      });
+    } else {
+      console.log("No user is signed in");
+    }
+  });
 }
-
